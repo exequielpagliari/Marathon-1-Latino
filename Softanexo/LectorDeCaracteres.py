602296ad31dcd2a498d2ec53b267f7870bc7d8d6
Exequiel Pagliari <58015928+exequielpagliari@users.noreply.github.com>
2023-06-05 16:25:58 -0300

    Modificadas via programación las terminales

---
+++ Softanexo/LectorDeCaracteres.py
@@ -0,0 +1,12 @@
+
+import codecs
+
+term = 'ter' + str(input("Ingrese nombre del Terminal a traducir:       ter"))
+
+with codecs.open(r'Terminales Traducidas\{}'.format(term), 'r', encoding='latin-1') as fin:
+    file_content = fin.read()
+
+
+
+with codecs.open(r'Terminales Traducidas\{}.txt'.format(term), 'x', encoding='macroman') as fout:    
+    fout.write(file_content)
